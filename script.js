90ae6360870bb2c55f2d3d6e38bb6551c6469ef2
Nate Berkopec <nate.berkopec@gmail.com>
2021-10-13 17:05:16 -0600

    Working nginx config + http2 comparison

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
+// import http from 'k6/http';
+// export let options = {
+//   discardResponseBodies: false,
+//   scenarios: {
+//     contacts: {
+//       executor: 'per-vu-iterations',
+//       vus: 6, // 6 connections per domain in HTTP 1
+//       iterations: 1,
+//       maxDuration: '1h30m',
+//     },
+//   },
+// };
+
+// // A little jank - is there a better way to get the URL from rails into this script?
+// export default function () {
+//   // The number of downloads here is based on real numbers from a 7+ year old Rails app
+//   for (let step = 1; step <= 200; step++) { // 1200 total downloads of a ~30kb file.
+//     http.get('http://localhost:3000/assets/es-module-shims.min-fb6f777d3001ba8b2a4d89d3b4c33deeb216fcdd610d55eb876ea72ad5ba50ef.js');
+//   }
+// }
+
+// http/2 version
+
 import http from 'k6/http';
 export let options = {
   discardResponseBodies: false,
   scenarios: {
     contacts: {
       executor: 'per-vu-iterations',
-      vus: 6, // 6 connections per domain in HTTP 1
+      vus: 1,
       iterations: 1,
       maxDuration: '1h30m',
     },
@@ -14,7 +37,7 @@ export let options = {
 // A little jank - is there a better way to get the URL from rails into this script?
 export default function () {
   // The number of downloads here is based on real numbers from a 7+ year old Rails app
-  for (let step = 1; step <= 200; step++) { // 1200 total downloads of a ~30kb file.
-    http.get('http://localhost:3000/assets/es-module-shims.min-fb6f777d3001ba8b2a4d89d3b4c33deeb216fcdd610d55eb876ea72ad5ba50ef.js');
+  for (let step = 1; step <= 1200; step++) { // 1200 total downloads of a ~30kb file.
+    http.get('https://localhost:9291/assets/es-module-shims.min-fb6f777d3001ba8b2a4d89d3b4c33deeb216fcdd610d55eb876ea72ad5ba50ef.js');
   }
 }
